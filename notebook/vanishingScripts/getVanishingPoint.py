ba55f19ffc83ae3813c5847540594f541975f0fe
DavideLigari01 <davide.ligari01@universitadipavia.it>
2023-11-03 11:51:04 +0100

    working on finding the vanishing lines

--- notebook/vanishingScripts/getVanishingPoint.py
+++ notebook/vanishingScripts/getVanishingPoint.py
@@ -1,40 +1,49 @@
 import math
+import random
 
 
-def getVanishingPoint(Lines):
-    # We will apply RANSAC inspired algorithm for this. We will take combination
-    # of 2 lines one by one, find their intersection point, and calculate the
-    # total error(loss) of that point. Error of the point means root of sum of
-    # squares of distance of that point from each line.
-    VanishingPoint = None
-    MinError = 100000000000
+def getVanishingPoint(Lines, max_iterations=10000, inlier_threshold=10):
+    """TO CHANGE: This function is used to find the vanishing point of a set of lines.
+    The idea is to find the intersection point of all possible pairs of lines.
+    and for each intersection point, count the number of lines that are close to it.
+    The intersection point with the highest number of inliers is the vanishing point and
+    the lines that are close to it are the vanishing lines."""
+    best_vanishing_point = None
+    best_inlier_count = 0
+    vanishing_lines = []
 
-    for i in range(len(Lines)):
-        for j in range(i + 1, len(Lines)):
-            m1, c1 = Lines[i][4], Lines[i][5]
-            m2, c2 = Lines[j][4], Lines[j][5]
+    for _ in range(max_iterations):
+        # Randomly sample two lines
+        sample_lines = random.sample(Lines, 2)
+        m1, c1 = sample_lines[0][4], sample_lines[0][5]
+        m2, c2 = sample_lines[1][4], sample_lines[1][5]
 
-            if m1 != m2:
-                x0 = (c1 - c2) / (m2 - m1)
-                y0 = m1 * x0 + c1
+        if m1 != m2:
+            # Calculate the intersection point
+            x0 = (c1 - c2) / (m2 - m1)
+            y0 = m1 * x0 + c1
 
-                err = 0
-                for k in range(len(Lines)):
-                    m, c = Lines[k][4], Lines[k][5]
-                    m_ = -1 / m
-                    c_ = y0 - m_ * x0
+            inlier_count = 0
 
-                    x_ = (c - c_) / (m_ - m)
-                    y_ = m_ * x_ + c_
+            # Count inliers (lines that are close to the intersection point)
+            for line in Lines:
+                m, c = line[4], line[5]
+                m_ = -1 / m
+                c_ = y0 - m_ * x0
 
-                    l = math.sqrt((y_ - y0) ** 2 + (x_ - x0) ** 2)
+                x_ = (c - c_) / (m_ - m)
+                y_ = m_ * x_ + c_
 
-                    err += l**2
+                distance = math.sqrt((y_ - y0) ** 2 + (x_ - x0) ** 2)
 
-                err = math.sqrt(err)
+                if distance < inlier_threshold:
+                    inlier_count += 1
 
-                if MinError > err:
-                    MinError = err
-                    VanishingPoint = [x0, y0]
+            if inlier_count > best_inlier_count:
+                best_inlier_count = inlier_count
+                best_vanishing_point = [x0, y0]
+                vanishing_lines.clear()
+                vanishing_lines.append(sample_lines[0])
+                vanishing_lines.append(sample_lines[1])
 
-    return VanishingPoint
+    return best_vanishing_point, vanishing_lines
