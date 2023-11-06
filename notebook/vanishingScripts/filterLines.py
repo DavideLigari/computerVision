4d14319513b873c71ea4e4c5778c83fb96015be5
DavideLigari01 <davide.ligari01@universitadipavia.it>
2023-11-06 17:35:40 +0100

    commented all functions

--- notebook/vanishingScripts/filterLines.py
+++ notebook/vanishingScripts/filterLines.py
@@ -4,37 +4,57 @@ import math
 REJECT_DEGREE_TH = 6.0
 
 
+# Function to filter and process lines
 def filterLines(lines):
-    finalLines = []
+    """
+    Filter and process lines based on slope and length.
+
+    Args:
+       * lines (list of numpy.ndarray): List of detected lines, where each element is an array containing line endpoints.
+
+    Returns:
+        list of numpy.ndarray: Filtered and processed lines.
+    """
+    finalLines = []  # Initialize an empty list to store the filtered lines
 
     for line in lines:
-        [[x1, y1, x2, y2]] = line
+        [[x1, y1, x2, y2]] = line[:3]
 
-        # Calculating equation of the line: y = mx + c
+        # Calculating the equation of the line: y = mx + c
         if x1 != x2:
-            m = (y2 - y1) / (x2 - x1)
+            m = (y2 - y1) / (x2 - x1)  # Calculate the slope (m) of the line
         else:
-            m = 100000000
-        c = y2 - m * x2
-        # theta will contain values between -90 -> +90.
+            m = 100000000  # A very large value to represent vertical lines
+        c = y2 - m * x2  # Calculate the y-intercept (c) of the line
+
+        # Calculate the angle (theta) of the line with respect to the horizontal
+        # Theta will contain values between -90 degrees to +90 degrees.
         theta = math.degrees(math.atan(m))
 
-        # Rejecting lines of slope near to 0 degree or 90 degree and storing others
+        # Rejecting lines of slope near to 0 degree or 90 degrees and storing others
+        # Lines with slopes close to 0 or 90 degrees are rejected as they are considered horizontal or vertical lines.
         if REJECT_DEGREE_TH <= abs(theta) <= (90 - REJECT_DEGREE_TH):
-            l = math.sqrt((y2 - y1) ** 2 + (x2 - x1) ** 2)  # length of the line
+            l = math.sqrt(
+                (y2 - y1) ** 2 + (x2 - x1) ** 2
+            )  # Calculate the length of the line
+
             notParallel = True
             for line2 in finalLines:
                 if line2[4] == m:
                     notParallel = False
+            # Check if a line with the same slope (parallel) already exists in finalLines
             if notParallel:
-                finalLines.append([x1, y1, x2, y2, m, c, l])
+                finalLines.append(
+                    [x1, y1, x2, y2, m, c, l]
+                )  # Store the line's endpoints, slope, intercept, and length
 
     # Removing extra lines
-    # (we might get many lines, so we are going to take only longest 15 lines
-    # for further computation because more than this number of lines will only
-    # contribute towards slowing down of our algo.)
-    if len(finalLines) > 5:
-        finalLines = sorted(finalLines, key=lambda x: x[-1], reverse=True)
-        finalLines = finalLines[:10]
+    # We might get many lines, so we are going to take only the longest 10 lines
+    # for further computation because more than this number of lines will only contribute towards slowing down the algorithm.
+    if len(finalLines) > 10:
+        finalLines = sorted(
+            finalLines, key=lambda x: x[-1], reverse=True
+        )  # Sort lines by length in descending order
+        finalLines = finalLines[:10]  # Keep only the top 10 longest lines
 
     return finalLines
--- notebook/vanishingScripts/findOptimalCannyParams.py
+++ notebook/vanishingScripts/findOptimalCannyParams.py
@@ -1,12 +1,23 @@
-# import the necessary packages
 import numpy as np
 
 
 def findOptimalCannyParams(image, sigma=0.22):
-    # compute the median of the single channel pixel intensities
+    """
+    Compute optimal Canny edge detection parameters for a given image.
+
+    Args:
+       * image (numpy.ndarray): The input image for edge detection.
+       * sigma (float): A parameter for adjusting the lower and upper Canny thresholds.
+
+    Returns:
+        int, int: The lower and upper Canny thresholds for edge detection.
+    """
+    # Compute the median of the single channel pixel intensities
     v = np.median(image)
-    # apply automatic Canny edge detection using the computed median
+
+    # Compute the lower and upper Canny thresholds using the computed median
     lower = int(max(0, (1.0 - sigma) * v))
     upper = int(min(255, (1.0 + sigma) * v))
-    # return the edged image
+
+    # Return the computed lower and upper thresholds
     return lower, upper
